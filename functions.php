67b4db735becf721ca320bb0f91d438ed1bb504e
jonburdon <jon@branchoutwebsites.com>
2019-09-11 10:25:26 +0100

    Enqueue style.css

--- functions.php
+++ functions.php
@@ -4,9 +4,14 @@
 function load_stylesheets()
 {
 
-    wp_register_style('stylesheet', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_register_style('style', get_template_directory_uri() . '/style.css', array(), false, 'all');
 
-    wp_enqueue_style('stylesheet');
+    wp_enqueue_style('style');
+
+
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+
+    wp_enqueue_style('bootstrap');
 
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
