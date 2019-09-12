93957b53bb2c37131187995d06f1e35ce67eb958
jonburdon <jon@branchoutwebsites.com>
2019-09-12 09:42:09 +0100

    Add bootstrap card classes to archive.php

--- archive.php
+++ archive.php
@@ -6,10 +6,13 @@
 
 <?php if (have_posts()) : while(have_posts()) : the_post();?>
 
-    <h3><?php the_title();?></h3>
-    <?php the_excerpt();?>
-    <a href="<?php the_permalink();?>" class="btn btn-success">Read more</a>
-    
+    <div class="card">
+        <div class="card-body">
+        <h3><?php the_title();?></h3>
+        <?php the_excerpt();?>
+        <a href="<?php the_permalink();?>" class="btn btn-success">Read more</a>
+        </div>
+    </div>    
 
 <?php endwhile; endif;?>
 
